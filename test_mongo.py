6f66ab16b6778a1d5aa1bd57f12ce995f72bfacb
M.K. Akindu Kalhan <kalhanmka.22@uom.lk>
2025-12-10 18:13:00 +0530

    cleaned unneccessary test files

--- test_mongo.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://root:rootpassword123@127.0.0.1:27017/admin")
-
-db = client.test_db
-collection = db.test_collection
-
-collection.insert_one({"name": "RouteGo"})
-print(list(collection.find()))
